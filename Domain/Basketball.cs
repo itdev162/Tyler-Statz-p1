179062d9d6a9207b34a0ac6a09f35c2b809b4cae
statzt <60333777+statzt@users.noreply.github.com>
2020-10-21 16:06:11 -0500

    Project 1

---
+++ Domain/Basketball.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Domain
+{
+    public class Basketball
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        
+    }
+}
